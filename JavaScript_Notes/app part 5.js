eb35f6b4254ce027dc5cc80a9e7e835205bef001
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-06-16 21:26:30 +0530

    add code

---
+++ JavaScript_Notes/app part 5.js
@@ -0,0 +1,105 @@
+// let delhi = {
+//  name:"krushna",
+//  age:23,
+//  education:"under graduation",
+//  colleg:"government colleg of engineering yavatmal",
+//  branch:"electronics and telicummunication engineering",
+ 
+// };
+
+// const student={
+//     name:"krushna",
+//     class:12,
+//     sub:["math","physics","chemistry"]
+// };
+// console.log(student);
+
+// const post={
+//     username:"@krushna",
+//     content:"this is my #firstpost",
+//     like:50,
+//     repost:2,
+//     tags:["@rani","@shilpa"]
+// };
+// console.log(post);
+
+// add or update value
+
+// const info={
+//     name:"krushna",
+//     marks:100,
+//     grade:"A",
+//     city:"delhi"
+// };
+
+// Nested objects 
+
+// const classInfo={
+//     hariom:{
+//         Blood_group:"A+",
+//         city:"Mehakr",
+//     },
+//     krushna:{
+//         Blood_group:"B+",
+//         city:"Washim"
+//     },
+//     kashish:{
+//         Blood_group:"O+",
+//         city:"Nagpur"
+//     }
+// };
+
+// Array of Object
+
+// let arr=[
+//     {
+//         name:"krushna",
+//         fild:"engineering"
+//     },
+//     {
+//         name:"hariom",
+//         fild:"engineering"
+//     },
+//     {
+//         name:"shilpa",
+//         fild:"B ed"
+//     }
+// ];
+
+
+// Assignment No 5
+
+// Q1.Create a program for generate the random number represent a dice roll
+
+// const max=6;
+// const random=Math.floor(Math.random()*max)+1;
+// console.log(random);
+//const guess=prompt("guess any number");
+// while(true){
+//     if(guess==random){
+//         console.log("y")
+//     }
+// }
+
+// Q2.Create an object representing a car that stores the following 
+// properties for the car:name,model,color
+
+// let car={
+//     name:"thar",
+//     model:2022,
+//     color:["black","white"]
+// };
+
+// Q3.Create an object Person with their name,age and city.
+// Edit their citys original value to change it to “NewYork”.
+// Add a new property country and set it to the United States 
+
+// const person={
+//     name:"krushna",
+//     age:22,
+//     city:"yavatmal"
+// };
+// person.city="NewYork"
+// person.country="United States"
+// console.log(person);
+
